89e3545b89c4cdd940b740f486f4a15207a7de45
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-09-30 17:07:09 +0500

    products

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
-import Image from 'next/image'
+const fetchProducts = async () => {
+  const res = await fetch('http://localhost:3000/api/products')
+  const product = await res.json()
+  return product
+}
 
-export default function Home() {
+export default async function Home() {
+  const products = await fetchProducts();
+  console.log("products", products)
   return (
-    <>hi
+    <>
+
+      {
+        products.products.map((item:{title:String})=>{
+          return <p>{item.title}</p>
+        })}
     </>
   )
 }
